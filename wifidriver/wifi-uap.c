71de3b33c800fb205da790cafdf38141f763480d
Sandeep Mane <sandeep.mane@nxp.com>
2022-03-04 21:40:21 +0530

    Issue: WCSWREL-280 Buildtest warnings-evkmimxrt1060-wifi_cli updated patch
    [Issue]
    Compilation warnings faced by IAR to compile the wifi_cli project of RT1060.
    
    [Fix]
    Resolved the warnings with appropriate changes. Changes done as to make
    left hand operand is same as that of the right operand.
    
    [Test]
    Compile and Build successful with RT1060 & 8987 wifi_cli on IAR,Keil
    & Armgcc.(0 Error(s), 0 Warning(s)).Scan and Connect as STA.
    uAP start and external station connect and ping.
    
    [Open Issues/Todo]
    No
    
    [Reviewers]
    Abhishek Misra,Sandip Dalvi, Supriya Inamdar, Abhinav Kulkarni, Sanjay Yadav
    
    Signed-off-by: Sandeep Mane <sandeep.mane@nxp.com>

--- wifidriver/wifi-uap.c
+++ wifidriver/wifi-uap.c
@@ -776,15 +776,10 @@ int wifi_uap_start(int type,
 {
     wuap_d("Configuring");
     /* Configure SSID */
-    int rv = wifi_cmd_uap_config(ssid, mac_addr, security, passphrase, password, channel, scan_chan_list,
-                                 wm_wifi.beacon_period == 0U ? UAP_BEACON_PERIOD : wm_wifi.beacon_period,
-                                 wm_wifi.bandwidth == 0U ? BANDWIDTH_40MHZ : wm_wifi.bandwidth,
-#ifdef CONFIG_WIFI_DTIM_PERIOD
-                                 dtim == 0? UAP_DTIM_PERIOD : dtim,
-#else
-                                 UAP_DTIM_PERIOD,
-#endif
-                                 wm_wifi.chan_sw_count, type);
+    int rv = wifi_cmd_uap_config(
+        ssid, mac_addr, (enum wlan_security_type)security, passphrase, password, channel, scan_chan_list,
+        wm_wifi.beacon_period == 0U ? UAP_BEACON_PERIOD : wm_wifi.beacon_period,
+        wm_wifi.bandwidth == 0U ? BANDWIDTH_40MHZ : wm_wifi.bandwidth, UAP_DTIM_PERIOD, wm_wifi.chan_sw_count, type);
     if (rv != WM_SUCCESS)
     {
         wuap_e("config failed. Cannot start");
