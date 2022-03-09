c32fb4777638b631199eb3433c76c8ed9713c8a0
Keval Thakker <keval.thakker@nxp.com>
2022-03-09 11:45:51 +0530

    Issue: WCSWREL-284 [2.11.1 Minor RFP RC1][WIFI&LWIP][88W8977]Failed to start AP.
    [Issue]
    Failed to start uAP after bandwidth set to 40M.
    
    [Fix]
    Removed the option provided for setting bandwidth to 40M, since 8977 firmware does not support 40 MHz bandwidth.
    The same option has been removed for 8801 as well.
    
    [Validation done]
    Check the help options.
    Setup uAP and verified that other stations are able to connect.
    
    [Open Issues/Todo]
    None.
    
    [Reviewers]
    Sandip Dalvi, Supriya Inamdar, Abhinav Kulkarni, Sachin Patki
    
    Signed-off-by: Keval Thakker <keval.thakker@nxp.com>

--- wlcmgr/wlan_tests.c
+++ wlcmgr/wlan_tests.c
@@ -1707,6 +1707,7 @@ static void test_wlan_8801_enable_ext_coex(int argc, char **argv)
 }
 #endif
 
+#if !defined(SD8977) && !defined(SD8801)
 static void test_wlan_set_uap_bandwidth(int argc, char **argv)
 {
     uint8_t bandwidth;
@@ -1730,6 +1731,7 @@ static void test_wlan_set_uap_bandwidth(int argc, char **argv)
     else
         (void)PRINTF("bandwidth set successfully\r\n");
 }
+#endif
 
 static struct cli_command tests[] = {
     {"wlan-scan", NULL, test_wlan_scan},
@@ -1762,7 +1764,9 @@ static struct cli_command tests[] = {
 #endif
     {"wlan-host-sleep", "<0/1> wowlan_test <0/1>", test_wlan_host_sleep},
     {"wlan-send-hostcmd", NULL, test_wlan_send_hostcmd},
+#if !defined(SD8977) && !defined(SD8801)
     {"wlan-set-uap-bandwidth", "<1/2> 1:20 MHz 2:40MHz", test_wlan_set_uap_bandwidth},
+#endif
 #ifdef SD8801
     {"wlan-8801-enable-ext-coex", NULL, test_wlan_8801_enable_ext_coex},
 #endif
