0e3a3620116ff20fcd493c0f2de59f736a3eb1a4
Keval Thakker <keval.thakker@nxp.com>
2022-05-17 13:11:29 +0530

    Issue: WSWRPR-1314 EAR 3.0 release for RT + Firecrest
    Resolving build error observed after creating new branch and rebasing it with p5.
    
    Reviewers: Abhinav K, Sandip D, Supriya I.
    
    Signed-off-by: Keval Thakker <keval.thakker@nxp.com>

--- wifidriver/incl/mlan_11ac.h
+++ wifidriver/incl/mlan_11ac.h
@@ -34,7 +34,7 @@ int wlan_cmd_append_11ac_tlv(mlan_private *pmpriv, BSSDescriptor_t *pbss_desc, t
 mlan_status wlan_11ac_cfg_ioctl(pmlan_adapter pmadapter, pmlan_ioctl_req pioctl_req);
 void wlan_update_11ac_cap(mlan_private *pmpriv);
 #endif
-t_u8 wlan_11ac_bandconfig_allowed(mlan_private *pmpriv, t_u16 bss_band);
+t_u8 wlan_11ac_bandconfig_allowed(mlan_private *pmpriv, mlan_band_def bss_band);
 
 mlan_status wlan_cmd_11ac_cfg(IN pmlan_private pmpriv,
                               IN HostCmd_DS_COMMAND *cmd,
