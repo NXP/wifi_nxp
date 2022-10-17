b1e9cc126ec820a22cdb59da8b1fa37980e8cf66
MCUSDK Jenkins CI <mcu_sdk_ci_jenkins@nxp.com>
2022-10-17 11:04:52 +0000

    Update the wifi content for mw320 2.9.4 release

---
+++ middleware_wifi_cli.cmake
@@ -0,0 +1,24 @@
+#Description: NXP WiFi CLI; user_visible: True
+include_guard(GLOBAL)
+message("middleware_wifi_cli component is included.")
+
+target_sources(${MCUX_SDK_PROJECT_NAME} PRIVATE
+    ${CMAKE_CURRENT_LIST_DIR}/cli/cli.c
+    ${CMAKE_CURRENT_LIST_DIR}/cli/cli_mem_simple.c
+    ${CMAKE_CURRENT_LIST_DIR}/cli/cli_utils.c
+    ${CMAKE_CURRENT_LIST_DIR}/nw_utils/ping.c
+    ${CMAKE_CURRENT_LIST_DIR}/nw_utils/iperf.c
+    ${CMAKE_CURRENT_LIST_DIR}/wlcmgr/wlan_basic_cli.c
+    ${CMAKE_CURRENT_LIST_DIR}/wlcmgr/wlan_tests.c
+    ${CMAKE_CURRENT_LIST_DIR}/wlcmgr/wlan_test_mode_tests.c
+    ${CMAKE_CURRENT_LIST_DIR}/dhcpd/dhcp-server-cli.c
+)
+
+target_include_directories(${MCUX_SDK_PROJECT_NAME} PUBLIC
+    ${CMAKE_CURRENT_LIST_DIR}/cli
+    ${CMAKE_CURRENT_LIST_DIR}/incl
+    ${CMAKE_CURRENT_LIST_DIR}/incl/wlcmgr
+)
+
+
+include(middleware_wifi)
