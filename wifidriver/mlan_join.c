21669b97e9b9bfda08669c46dbcf62d22f66d155
Keval Thakker <keval.thakker@nxp.com>
2022-12-09 18:32:25 +0530

    Issue: WSWRPR-1759 [1170][9177] Resolving build errors and stabilizing the new release branch(develop/2.13.0_firecrest) for Firecrest
    [Issue]
    Build warnings observed in mcuXpresso and armgcc compilers.
    
    [Issue Analysis]
    Structure _MrvlIEtypesHeader was packed so type casting pointers to it's member variables could cause problem.
    
    [Fix]
    Added a local variable and replaced it instead of the packed variable that was causing the problem.
    Ran and applied file formatting scripts.
    
    [Validation done]
    Build wifi_cli application successfully with mcuXpresso, armgcc, IAR and MDK compilers.
    
    [Open Issues/Todo]
    None.
    
    [Reviewers]
    Abhinav Kulkarni, Sandip Dalvi, Supriya Inamdar, Sanjay Yadav.
    
    Signed-off-by: Keval Thakker <keval.thakker@nxp.com>

--- wifidriver/mlan_join.c
+++ wifidriver/mlan_join.c
@@ -988,8 +988,10 @@ mlan_status wlan_cmd_802_11_associate(IN mlan_private *pmpriv, IN HostCmd_DS_COM
                 (void)__memcpy(pmadapter, prsn_ie_tlv->rsn_ie, &pmpriv->wpa_ie[2], prsn_ie_tlv->header.len);
                 if (pmpriv->sec_info.wpa2_enabled)
                 {
-                    akm_type = pauth_tlv ? wlan_le16_to_cpu(pauth_tlv->auth_type) : AssocAgentAuth_Auto;
-                    ret      = wlan_update_rsn_ie(pmpriv, prsn_ie_tlv, (t_u16 *)&prsn_ie_tlv->header.len, &akm_type);
+                    akm_type                = pauth_tlv ? wlan_le16_to_cpu(pauth_tlv->auth_type) : AssocAgentAuth_Auto;
+                    t_u16 rsn_ie_tlv_len    = prsn_ie_tlv->header.len;
+                    ret                     = wlan_update_rsn_ie(pmpriv, prsn_ie_tlv, &rsn_ie_tlv_len, &akm_type);
+                    prsn_ie_tlv->header.len = rsn_ie_tlv_len;
 #ifdef CONFIG_11R
                     /** parse rsn ie to find whether ft akm is used*/
                     ft_akm = wlan_ft_akm_is_used(pmpriv, (t_u8 *)prsn_ie_tlv);
