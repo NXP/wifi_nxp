95cab06b26674e170ada9c54253e391348763fe6
devesh yadav <devesh.yadav@nxp.com>
2023-02-10 16:30:34 +0530

    Issue: WSW-25308 [RT1170][9177] Enable Enhanced CLI
    [Issue]
    enabling the enhanced-CLI to get commands present in all other apps into the wifi-cli app
    
    [FIX]
    added the files into wlcmgr to make it common for different apps
    updated the copyright
    added host_sleep to common folder and created function for the same to be called
    deleted the host_sleep files to move it into the source directory as it has to be at application level
    
    [Validation done]
    locally created a bin after the above changes, commands were found present
    
    [Open Issues/Todo]
    Once the package is generated, will check the commands and update further.
    
    [Reviewers]
    Abhinav Kulkarni, Sandip Dalvi, Supriya Inamdar, Sanjay Yadav

---
+++ incl/wlcmgr/wlan_features.h
@@ -0,0 +1,21 @@
+/*
+ *  Copyright 2021-2023 NXP
+ *  All rights reserved.
+ *
+ *  SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#ifdef CONFIG_WIFI_FEATURES
+/** Register WLAN Features CLI commands.
+ *
+ *  Register the WLAN Features CLI commands offload and hostwake features.
+ *
+ *  \note This function can only be called by the application after \ref wlan_init()
+ *  called.
+ *
+ *  \return WM_SUCCESS if the CLI commands were registered or
+ *  \return -WM_FAIL if they were not (for example if this function
+ *           was called while the CLI commands were already registered).
+ */
+int wlan_features_cli_init(void);
+#endif
