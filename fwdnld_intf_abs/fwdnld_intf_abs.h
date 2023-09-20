24aeed06b4fb9bd0e896a49ea4cbe3b68f1056a7
Vikram Hande <vikram.khande@nxp.com>
2023-09-20 15:33:53 +0530

    Issue: WCSWREL-490 Update and Resolve build errors on develop/sdk_api_2.x
    [Issue]
    Update and Resolve build errors on develop/sdk_api_2.x
    
    [Fix]
    Added api declaration for firmware download.
    
    [Validation done]
    Verified compilation.
    
    [Open Issues/Todo]
    None
    
    [Reviewers]
    Sanjay Yadav, Sandip Dalvi, Abhinav Kulkarni, Supriya Inamdar, Pankaj Razdan
    
    Signed-off-by: Vikram Hande <vikram.khande@nxp.com>

--- fwdnld_intf_abs/fwdnld_intf_abs.h
+++ fwdnld_intf_abs/fwdnld_intf_abs.h
@@ -80,4 +80,19 @@ typedef struct fwdnldintf
 #define FW_RELOAD_SDIO_HW_RESET 5
 #endif
 
+/** API to init the interface
+ *
+ * This function calls internally the interface specific init
+ *
+ * \param[in] intf_type Its of type fwdnld_intf_type_t
+ * \param[in] intf_specific Initialisation sequence related any setting that
+ * the specific interface needs.
+ * \note intf_specific is to be interpretted and defined by each interface
+ *
+ * \return fwdnld_intf* Success, a pointer to interface is returned. To be used in
+ * further API calls to this inited interface.
+ * \return In case init fails, NULL is returned.
+ */
+fwdnld_intf_t *fwdnld_intf_init(fwdnld_intf_type_t intf_type, void *intf_specific);
+
 #endif /*_FWDNLD_INTF_ABS_H_*/
