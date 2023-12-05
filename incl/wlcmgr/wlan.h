f6022456240a3349420aa2ab7b39e5bc94632bc8
Gang Li <gang.li_1@nxp.com>
2023-12-05 17:56:15 +0900

    Issue: WSW-35009 Add CONFIG_MAX_RESCAN_LIMIT macro to make WLAN_RESCAN_LIMIT configurable
    [Issue]
    [Honeywell][RW610][SF#00581107] Reconnection with AP is not established. Scanning is stopped after a few attempts
    
    [Fix]
    Modify the macro WLAN SCAN LIMIT to keep scanning until connected.
    Add CONFIG_MAX_RESCAN_LIMIT macro to configure WLAN_RESCAN_LIMIT.
    
    [Validation done]
    Keep scanning until reconnection is successful.
    
    [Open Issues/Todo]
    None
    
    [Reviewers]
    Gannicus Guo, Sandip Dalvi, Hui Bai, Maochen Wang, Li Long
    
    Signed-off-by: Gang Li <gang.li_1@nxp.com>

--- incl/wlcmgr/wlan.h
+++ incl/wlcmgr/wlan.h
@@ -184,6 +184,9 @@ typedef enum
 
 /** The number of times that the WLAN Connection Manager will look for a
  *  network before giving up. */
+#ifdef CONFIG_MAX_RESCAN_LIMIT
+#define WLAN_RESCAN_LIMIT CONFIG_MAX_RESCAN_LIMIT
+#else
 #ifdef CONFIG_WPA_SUPP
 #define WLAN_RESCAN_LIMIT 30U
 #else
@@ -191,8 +194,9 @@ typedef enum
 #define WLAN_RESCAN_LIMIT 10U
 #else
 #define WLAN_RESCAN_LIMIT 5U
-#endif
+#endif /* CONFIG_P2P */
 #endif /* CONFIG_WPA_SUPP */
+#endif /* CONFIG_MAX_RESCAN_LIMIT */
 
 #define WLAN_11D_SCAN_LIMIT 3U
 /** The number of times that the WLAN Connection Manager will attempt a
