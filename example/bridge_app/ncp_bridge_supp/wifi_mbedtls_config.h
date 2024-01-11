df3e1583ee1bc459d9524e1ef8d45d5aef0dd834
nxf93650 <gaofeng.zhang@nxp.com>
2024-01-11 08:11:29 +0100

    Issue: WSW-35720 fix https connect fail beacause of the undef MBEDTLS_SSL_BUFFER_SIZES
    [Issue]
    Issue: fix https connect fail beacause of the undef
    MBEDTLS_SSL_BUFFER_SIZES
    
    [Fix]
    fix https connect fail beacause of the undef MBEDTLS_SSL_BUFFER_SIZES
    
    [Validation done]
    wlan-http-connect https://192.168.50.189
    
    [Open Issues/Todo]
    None
    
    [Reviewers]
    Gannicus Guo, Sandip Dalv

---
+++ example/bridge_app/ncp_bridge_supp/wifi_mbedtls_config.h
@@ -0,0 +1,39 @@
+/**
+ * \file config.h
+ *
+ * \brief Configuration options (set of defines)
+ *
+ *  This set of compile-time options may be used to enable
+ *  or disable features selectively, and reduce the global
+ *  memory footprint.
+ */
+/*
+ *  Copyright The Mbed TLS Contributors
+ *  SPDX-License-Identifier: Apache-2.0
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License"); you may
+ *  not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ *  WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+#ifndef __MBEDTLS_NCP_CONFIG__
+#define __MBEDTLS_NCP_CONFIG__
+
+/**
+ * \def MBEDTLS_SSL_BUFFER_SIZES
+ *
+ * Uncomment this macro in case CCM should be used with SSL buffer size calling CSS IP.
+ *
+ */
+#define MBEDTLS_SSL_BUFFER_SIZES
+
+
+#endif /* MBEDTLS_CONFIG_H */
