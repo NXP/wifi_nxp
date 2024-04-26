33cde6c3049c9737ad83d22d19196640b6cb8b3d
Mcu_sdk Ci <mcu_sdk.ci@nxp.com>
2024-04-26 14:28:08 +0200

    Pull request #36: Add files for release/2.15.001_rt1180_rfp_gh_review from CI Generation
    Merge in MCUCORE/mcux-sdk-wifi-nxp from release/2.15.001_rt1180_rfp_gh_review to release/2.15.001_rt1180_rfp_gh

--- wifi_bt_firmware/wlan_bt_fw.h
+++ wifi_bt_firmware/wlan_bt_fw.h
@@ -9,27 +9,27 @@
 #define __WLAN_BT_FW_H__
 
 #if defined(SD8801)
-#include "8801/sd8801_wlan.h"
+#include "sd8801_wlan.h"
 #elif defined(SD8978)
 #if !defined(CONFIG_WIFI_IND_DNLD) && !defined(CONFIG_BT_IND_DNLD)
-#include "IW416/sduartIW416_wlan_bt.h"
+#include "sduartIW416_wlan_bt.h"
 #else
-#include "IW416/sdIW416_wlan.h"
-#include "IW416/uartIW416_bt.h"
+#include "sdIW416_wlan.h"
+#include "uartIW416_bt.h"
 #endif
 #elif defined(SD8987)
 #if !defined(CONFIG_WIFI_IND_DNLD) && !defined(CONFIG_BT_IND_DNLD)
-#include "8987/sduart8987_wlan_bt.h"
+#include "sduart8987_wlan_bt.h"
 #else
-#include "8987/sd8987_wlan.h"
-#include "8987/uart8987_bt.h"
+#include "sd8987_wlan.h"
+#include "uart8987_bt.h"
 #endif
 #elif defined(SD9177)
 #if !defined(CONFIG_WIFI_IND_DNLD) && !defined(CONFIG_BT_IND_DNLD)
-#include "nw61x/sduart_nw61x_se.h"
+#include "sduart_nw61x_se.h"
 #else
-#include "nw61x/sd_nw61x_se.h"
-#include "nw61x/uart_nw61x_se.h"
+#include "sd_nw61x_se.h"
+#include "uart_nw61x_se.h"
 #endif
 #endif
 
