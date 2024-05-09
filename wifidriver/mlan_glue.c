NXP/wifi_nxp

commit bdf55b2f4c6f7e22c5aa0d04ff565eda872da77b

Issue: WSW-42075 Fix coverity out-of bound.
[Issue]
WSW-42075 Fix coverity out-of bound.

[Validation]
builds ok, wlan-add/scan/connect/uap/stop

[Open Issues/Todo]
None

[Reviewers]
Gannicus Guo, Qingling Wu

wifidriver/mlan_glue.c
@@ -6428,7 +6428,7 @@ int wrapper_bssdesc_first_set(int bss_index,
     if ((d->prsnx_ie != MNULL) && (d->prsnx_ie->data[0] & (0x1 << SAE_H2E_BIT)))
     {
         *ap_pwe = 2;
-        for (i = WLAN_SUPPORTED_RATES; i > 0; i--)
+        for (i = (WLAN_SUPPORTED_RATES - 1); i >= 0; i--)
         {
             if (d->data_rates[i] == 0xFB)
             {

wlcmgr/wlan.c
@@ -8019,7 +8019,7 @@ int wlan_start(int (*cb)(enum wlan_event_reason reason, void *data))
 
 int wlan_stop(void)
 {
-    osa_status_t status;
+    osa_status_t status = KOSA_StatusIdle;
     int ret = WM_SUCCESS;
 #ifndef RW610
     int total_wait_time = 1000; /* millisecs */

commit 694dbccf8aa23ced6dad2e55bdcbbe075c4120b8

Issue: WSW-43627 [STA][RW610][AP-IOT] Redfinch will hang after do whole channel scan with ARUBA 11AX Enterprise AP
[Analyze]
Regression from commit bdf55b2f4c6f7e22c5aa0d04ff565eda872da77b
In 'for (i = (WLAN_SUPPORTED_RATES - 1); i >= 0; i--)', 'i >= 0' (i is uint8_t) make the 'for' loop could not end and cause hang

[Fix]
Use 'i > 0' as the condition

[Validation done]
Local test OK.

[Todo]
None

[Reviewers]
Gannicus Guo, Sandip Dalve, Wenhui Jin, Pan Gao, Li Long, Gaofeng Zhang

Signed-off-by: Qingling Wu <qingling.wu@nxp.com>

wifidriver/mlan_glue.c
@@ -6367,7 +6367,7 @@ int wrapper_bssdesc_first_set(int bss_index,
                               t_u8 *ap_mfpr,
                               t_u8 *ap_pwe)
 {
-    int8_t i = WLAN_SUPPORTED_RATES;
+    uint8_t i = WLAN_SUPPORTED_RATES;
     if (bss_index >= (int)mlan_adap->num_in_scan_table)
     {
         wifi_w("Unable to find given entry %d in BSS table", bss_index);
@@ -6428,9 +6428,9 @@ int wrapper_bssdesc_first_set(int bss_index,
     if ((d->prsnx_ie != MNULL) && (d->prsnx_ie->data[0] & (0x1 << SAE_H2E_BIT)))
     {
         *ap_pwe = 2;
-        for (i = (WLAN_SUPPORTED_RATES - 1); i >= 0; i--)
+        for (i = WLAN_SUPPORTED_RATES; i > 0; i--)
         {
-            if (d->data_rates[i] == 0xFB)
+            if (d->data_rates[i-1] == 0xFB)
             {
                 *ap_pwe = 1;
                 break;