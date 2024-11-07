c2c861203e088d227064c11cb2cdde53b26fe397
Abhinav Kulkarni <abhinav.kulkarni@nxp.com>
2024-11-07 21:03:09 +0530

    Fixed Mutli-AP IE to add station command destined for firmware
    [Description]
    This issue might be seen when next IE will be added.
    
    [Root Cause]
    Issue not seen as of now but could be seen if new TLV is added.
    
    [Fix]
    Added incremental value to pos
    
    [Testing]
    Compilation successful
    
    Change-Id: d0d13370-0bc5-4fbb-8cfb-f48409f0b604
    
    Signed-off-by: Abhinav Kulkarni <abhinav.kulkarni@nxp.com>

--- wifidriver/mlan_uap_cmdevent.c
+++ wifidriver/mlan_uap_cmdevent.c
@@ -1198,6 +1198,7 @@ static mlan_status wlan_uap_cmd_add_station(pmlan_private pmpriv,
 
             pos += sizeof(MrvlIEtypesHeader_t);
             (void)__memcpy(NULL, pos, (t_u8 *)&sta_ptr->he_cap.ext_id, tlv->len);
+            pos += tlv->len;
             travel_len += sizeof(MrvlIEtypesHeader_t) + tlv->len;
         }
     }
