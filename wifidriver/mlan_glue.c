a5f7553783fc11fc7655085aa392d9b4d30ce05a
Maochen Wang <maochen.wang@nxp.com>
2024-12-02 06:31:20 +0800

    Fix connection failure when using 2.4G only chip.
    [Description]
    Failed to connect to EXT-APon 2.4GHz when using 2.4G only chip.
    
    [Root Cause]
    Before sending assoc request, will check the region channel and download countryinfo to FW for both 2.4G and 5G.
    But for 2.4G only chip, the check of region channel will fail, and return error before sending assoc request.
    
    [Fix]
    Need to check if supports 5G before checking the region channel.
    
    [Testing]
    Wi-Fi connection is successful when using 2.4G only chip.
    
    [Open Issues/Todo]
    None
    
    [Reviewers]
    Gannicus Guo
    
    [Port proposal]
    general
    
    Change-Id: 21b70910-b030-11ef-941d-5b57aea44277
    Signed-off-by: Maochen Wang <maochen.wang@nxp.com>

--- wifidriver/mlan_glue.c
+++ wifidriver/mlan_glue.c
@@ -2025,7 +2025,8 @@ int wrapper_wifi_assoc(
             }
 
 #if CONFIG_5GHz_SUPPORT
-            if (priv->support_11d->wlan_11d_create_dnld_countryinfo_p(priv, BAND_A) != MLAN_STATUS_SUCCESS)
+            if ((!ISSUPP_NO5G(mlan_adap->fw_cap_ext))
+                && (priv->support_11d->wlan_11d_create_dnld_countryinfo_p(priv, BAND_A) != MLAN_STATUS_SUCCESS))
             {
                 PRINTM(MERROR, "Dnld_countryinfo_11d failed\n");
                 return -WM_FAIL;
@@ -2604,7 +2605,8 @@ int wifi_nxp_send_assoc(nxp_wifi_assoc_info_t *assoc_info)
             }
 
 #if CONFIG_5GHz_SUPPORT
-            if (priv->support_11d->wlan_11d_create_dnld_countryinfo_p(priv, BAND_A) != MLAN_STATUS_SUCCESS)
+            if ((!ISSUPP_NO5G(mlan_adap->fw_cap_ext))
+                && (priv->support_11d->wlan_11d_create_dnld_countryinfo_p(priv, BAND_A) != MLAN_STATUS_SUCCESS))
             {
                 PRINTM(MERROR, "Dnld_countryinfo_11d failed\n");
                 return -WM_FAIL;
