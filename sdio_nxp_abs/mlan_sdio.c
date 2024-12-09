7e1f9c3ec21112a49a39fc91cc5787c72e6443eb
Sandip Dalvi <sandip.dalvi@nxp.com>
2024-12-09 15:02:29 +0530

    SDIO CIS BlockSize for FUNC0 is hardcoded to 256 instead of setting according to SoC
    [Description]
    SDIO CIS BlockSize for FUNC0 is hardcoded to 256 instead of setting according to SoC
    
    [Root Cause]
    Hardcoded block size value is set for FUNC 0 during SDIO init.
    
    [Fix]
    Change hardcoded block size of FUNC 0 to as per CIS register return value
    as per SoC.
    
    [Testing]
    All SoCs SDIO initialization working as before.
    
    Change-Id: 87fb44a8-b610-11ef-845c-2f14baff563e
    
    Signed-off-by: Sandip Dalvi <sandip.dalvi@nxp.com>

--- sdio_nxp_abs/mlan_sdio.c
+++ sdio_nxp_abs/mlan_sdio.c
@@ -320,7 +320,11 @@ static int sdio_card_init(void)
     /* Enable IO in card */
     (void)sdio_drv_creg_write(0x2, 0, 0x2, &resp);
 
+#if defined(SD9177) || defined(SD8978)
+    (void)SDIO_SetBlockSize(&wm_g_sd, (sdio_func_num_t)0, 1);
+#elif defined(SD8801) || defined(SD8987)
     (void)SDIO_SetBlockSize(&wm_g_sd, (sdio_func_num_t)0, 256);
+#endif
     (void)SDIO_SetBlockSize(&wm_g_sd, (sdio_func_num_t)1, 256);
     (void)SDIO_SetBlockSize(&wm_g_sd, (sdio_func_num_t)2, 256);
 
