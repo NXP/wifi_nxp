175d78aef4c0d32219efceeb2e3f29ead895fcbf
Maochen Wang <maochen.wang@nxp.com>
2024-12-10 14:53:02 +0800

    Only send WIFI_EVENT_UAP_NET_ADDR_CONFIG when UAP_SUPPORT is enabled
    [Description]
    For wifi_cli, disable UAP_SUPPORT, but still send WIFI_EVENT_UAP_NET_ADDR_CONFIG event.
    
    [Root Cause]
    Only send WIFI_EVENT_UAP_NET_ADDR_CONFIG when UAP_SUPPORT is enabled
    
    [Fix]
    Only send WIFI_EVENT_UAP_NET_ADDR_CONFIG when UAP_SUPPORT is enabled
    
    [Testing]
    Done
    
    [Open Issues/Todo]
    None
    
    [Reviewers]
    Gannicus Guo
    
    [Port proposal]
    general
    
    Change-Id: 59a11570-b6c3-11ef-b581-2f72db116ba7
    Signed-off-by: Maochen Wang <maochen.wang@nxp.com>

--- port/net/zephyr/net.c
+++ port/net/zephyr/net.c
@@ -1072,7 +1072,7 @@ static void wifi_net_event_handler(struct net_mgmt_event_callback *cb, uint32_t 
             break;
         case NET_EVENT_IPV6_DAD_SUCCEED:
             net_d("Receive zephyr ipv6 dad finished event.");
-#if !CONFIG_WIFI_NM_HOSTAPD_AP
+#if UAP_SUPPORT && !CONFIG_WIFI_NM_HOSTAPD_AP
             /*Wi-Fi driver will recevie NET_EVENT_IPV6_DAD_SUCCEED from zephyr kernel after IPV6 DAD finished.
             Can notify wlcmgr_task task to get address.*/
             (void)wlan_wlcmgr_send_msg(WIFI_EVENT_UAP_NET_ADDR_CONFIG, WIFI_EVENT_REASON_SUCCESS, NULL);
