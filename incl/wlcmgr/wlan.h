6f037551834fe50f924a91f24072af4261300f9a
Qingling Wu <qingling.wu@nxp.com>
2024-12-16 15:50:56 +0800

    Udpate Wi-Fi version to r48.p30
    [Description]
    Update the Wi-Fi driver version.
    
    [Root Cause]
    Update for release
    
    [Fix]
    Update driver version to r48.p30
    
    [Testing]
    None
    
    [Reviewers]
    Gannicus Guo, Sandip Dalvi, Pan Gao
    
    [Port proposal]
    non-general
    
    Change-Id: I90d31477aca31a22ba45a53581428c74360f91e2
    Signed-off-by: Qingling Wu <qingling.wu@nxp.com>

--- incl/wlcmgr/wlan.h
+++ incl/wlcmgr/wlan.h
@@ -22,7 +22,7 @@
 #include <wifi_events.h>
 #include <wifi.h>
 
-#define WLAN_DRV_VERSION "v1.3.r48.p29"
+#define WLAN_DRV_VERSION "v1.3.r48.p30"
 
 #if CONFIG_WPA2_ENTP
 #include <wm_mbedtls_helper_api.h>
