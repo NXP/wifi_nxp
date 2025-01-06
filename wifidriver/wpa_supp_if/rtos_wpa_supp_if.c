d9f68317f46ad376bde9477c1fc8bcfedcd0937b
rex.chen <rex.chen_1@nxp.com>
2025-01-06 11:12:25 +0800

    Fix memory leak during start uap
    [Description]
    A. [AP+STA] APUT fails to start after ~20+hrs Stress test- STA connected and AP ON/OFF stress.
    B. [AP+STA] Scan-connect-disconnect_AP-start-stop stress failed after ~12 Hrs with "reason [w_mem] Error: [evtbuf] Alloc: S: 1777 FAILED", STAUT fails to connect/APUT fails to start.
    
    [Root Cause]
    Memory leak in wifi_nxp_wpa_supp_set_country during start uap.
    
    [Fix]
    Use stack instead of heap to fix it.
    
    [Testing]
    Stres test more than 60 hours, heap info correct.
    
    Change-Id: 7acd4722862211efbde95b8c49729026
    
    Signed-off-by: rex.chen <rex.chen_1@nxp.com>

--- wifidriver/wpa_supp_if/rtos_wpa_supp_if.c
+++ wifidriver/wpa_supp_if/rtos_wpa_supp_if.c
@@ -1556,19 +1556,25 @@ int wifi_nxp_wpa_supp_set_country(void *if_priv, const char *alpha2)
 {
     struct wifi_nxp_ctx_rtos *wifi_if_ctx_rtos = NULL;
     int ret                                    = -WM_FAIL;
-    char *country                              = NULL;
+    char country[COUNTRY_CODE_LEN]             = {0};
     t_u8 region_code_rw610;
     unsigned char country3 = 0x20;
-
-    country = OSA_MemoryAllocate(COUNTRY_CODE_LEN);
-    (void)memcpy(country, alpha2, COUNTRY_CODE_LEN - 1);
-    country[2] = country3;
 
     if ((!if_priv) || (!alpha2))
     {
         supp_e("%s: Invalid params", __func__);
         goto out;
     }
+
+    if ((alpha2[2] == 0x4f) || (alpha2[2] == 0x49) || (alpha2[2] == 0x58) || (alpha2[2] == 0x04))
+    {
+        country3 = alpha2[2];
+    }
+
+    country[0] = alpha2[0];
+    country[1] = alpha2[1];
+    country[2] = country3;
+
     ret = wlan_11d_region_2_code(mlan_adap, (t_u8 *)country, &region_code_rw610);
     if(ret != WM_SUCCESS)
     {
