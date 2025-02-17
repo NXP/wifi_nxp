fda9ae870f93472b24df08b0b083de6073fe1100
Manav Chotalia <manavashwinbhai.chotalia@nxp.com>
2025-02-17 15:44:55 +0530

    Fixed STAUT-Legacy Roaming | TCP transmission getting aborted during roaming, when DUT roams from AP1 to AP2
    [Description]
    DUT failed to perform tcp transmission during roaming from AP1 to AP2
    
    [Root Cause]
    Due to Pre-asleep TCP-Tx failed to transmit and resulting in to aborted
    while roaming.
    
    [Fix]
    Disable Pre-asleep while fw init state.
    
    [Testing]
    Validated TCP-Tx stress while roaming.
    
    [Open Issues/Todo]
    None
    
    [Reviewers]
    Sandip Dalvi, Supriya Inamdar, Abhinav Kulkarni, keval Thakker
    
    Change-Id: b61ed3f6-ed18-11ef-bbcf-53b45c4a258e
    
    Signed-off-by: Manav Chotalia <manavashwinbhai.chotalia@nxp.com>

--- wifidriver/wifi_pwrmgr.c
+++ wifidriver/wifi_pwrmgr.c
@@ -32,6 +32,9 @@
 #define MIN_LISTEN_INTERVAL_IN_TU       50
 #define MAX_LISTEN_INTERVAL_IN_TU       65000
 #define CLOSEST_DTIM_TO_LISTEN_INTERVAL 65534
+/** To enable PreAsleep in firmware set CONFIG_ENABLE_PRESLEEP to '1' */
+/** **WAR** PreAsleep is disabled in driver due to issues seen on mcux toolchain. */
+#define CONFIG_ENABLE_PRESLEEP          0
 
 static bool ieeeps_enabled;
 static bool deepsleepps_enabled;
@@ -321,6 +324,9 @@ int wifi_exit_deepsleep_power_save(void)
 int wifi_set_power_save_mode(void)
 {
     t_u32 mode = BLOCK_CMD_IN_PRE_ASLEEP;
+#if !CONFIG_ENABLE_PRESLEEP
+    mode = (mode & ~BLOCK_CMD_IN_PRE_ASLEEP);
+#endif
 
     return wifi_send_power_save_command(EXT_PS_PARAM, 0U, MLAN_BSS_TYPE_STA, &mode);
 }
