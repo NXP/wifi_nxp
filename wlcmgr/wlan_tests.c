7e70e9a54dccc5033e7f4b3b62a3f136bfe066e6
Gang Li <gang.li_1@nxp.com>
2025-02-17 04:37:35 +0100

    Fix hardFault exception happen after send wlan-reset command without put argument
    [Description]
    [Coex]HardFault exception happen after send wlan-reset command without put argument(0/1/2) after boot up the board
    
    [Root Cause]
    test_wlan_reset() does not check before using input parameters.
    When test_wlan_reset()->atoi() accesses the address pointed to by the input parameter,
    it is an invalid memory and cannot be accessed, resulting in a hardfault.
    
    [Fix]
    Added input parameter checking.
    
    [Testing]
    Compilation is ok.
    
    [Reviewers]
    Gannicus Guo, Hui Bai
    
    Change-Id: I7d7a0127b4d1fd84ed7f475eb0f3ae9269ed1de2
    Signed-off-by: Gang Li <gang.li_1@nxp.com>

--- wlcmgr/wlan_tests.c
+++ wlcmgr/wlan_tests.c
@@ -7478,19 +7478,33 @@ static void test_wlan_set_mac_address(int argc, char **argv)
 
 #if (CONFIG_WIFI_RESET)
 #ifdef RW610
+static void dump_wlan_reset_usage(void)
+{
+    (void)PRINTF("Usage: wlan-reset <options>\r\n");
+    (void)PRINTF("0 to Disable WiFi\r\n");
+    (void)PRINTF("1 to Enable WiFi\r\n");
+    (void)PRINTF("2 to Reset WiFi\r\n");
+}
+
 static void test_wlan_reset(int argc, char **argv)
 {
     int option;
 
-    option = atoi(argv[1]);
-    if (argc != 2 || (option != 0 && option != 1 && option != 2))
+    if (argc != 2)
     {
-        (void)PRINTF("Usage: %s <options>\r\n", argv[0]);
-        (void)PRINTF("0 to Disable WiFi\r\n");
-        (void)PRINTF("1 to Enable WiFi\r\n");
-        (void)PRINTF("2 to Reset WiFi\r\n");
+        (void)PRINTF("Error: invalid number of arguments\r\n");
+        dump_wlan_reset_usage();
         return;
     }
+
+    option = atoi(argv[1]);
+    if (option != 0 && option != 1 && option != 2)
+    {
+        (void)PRINTF("Error: invalid option\r\n");
+        dump_wlan_reset_usage();
+        return;
+    }
+
 #if CONFIG_CSI
     if (option == 2)
     {
