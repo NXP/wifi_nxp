be8e480644b048d5f0d365d0850d0b9feab9c0ab
Gang Li <gang.li_1@nxp.com>
2025-03-07 06:02:20 +0100

    Fix 11n capability mask for 2.4GHz for IW610
    [Description]
    IW610 11n capability does not support 40MHz
    
    [Root Cause]
    IW610 is 20MHz only chip.
    
    [Fix]
    For IW610, 20MHz should be configured default.
    
    [Testing]
    Done
    
    [Open Issues/Todo]
    None
    
    [Reviewers]
    Gannicus Guo, Maochen Wang
    
    [Port proposal]
    non-general
    
    Change-Id: I1ebc52735d8f31d02b1e359f9b0286a1f091a032
    Signed-off-by: Gang Li <gang.li_1@nxp.com>

--- wifidriver/incl/mlan_fw.h
+++ wifidriver/incl/mlan_fw.h
@@ -513,8 +513,7 @@ typedef enum _WLAN_802_11_WEP_STATUS
 #define CAPINFO_40MHZ_INTOLARENT MBIT(8)
 
 /** Default 11n capability mask for 2.4GHz */
-#if defined(SD8978) || defined(SD8987) || defined(SD8997) || defined(SD9097) || defined(SD9098) || defined(SD9177) || \
-    defined(IW610)
+#if defined(SD8978) || defined(SD8987) || defined(SD8997) || defined(SD9097) || defined(SD9098) || defined(SD9177)
 #define DEFAULT_11N_CAP_MASK_BG \
     (HWSPEC_SHORTGI20_SUPP | HWSPEC_RXSTBC_SUPP | HWSPEC_SHORTGI40_SUPP | HWSPEC_CHANBW40_SUPP | HWSPEC_LDPC_SUPP)
 #elif defined(SD8801) || defined(RW610) || defined(IW610)
