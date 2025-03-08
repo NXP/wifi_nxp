7b827fe6e2465de7840f07089b368f7d74f034db
Maochen Wang <maochen.wang@nxp.com>
2025-03-08 11:48:09 +0800

    update wlan version to v1.3.r51.z_up.p3
    [Description]
    update wlan version to v1.3.r51.z_up.p3
    
    [Root Cause]
    update wlan version to v1.3.r51.z_up.p3
    
    [Fix]
    update wlan version to v1.3.r51.z_up.p3
    
    [Testing]
    Passed
    
    [Open Issues/Todo]
    None
    
    [Reviewers]
    Gannicus Guo
    
    [Port proposal]
    non-general
    
    Change-Id: 2145039c-fbd0-11ef-83ee-93f24ee16c5d
    Signed-off-by: Maochen Wang <maochen.wang@nxp.com>

--- incl/wlcmgr/wlan.h
+++ incl/wlcmgr/wlan.h
@@ -19,7 +19,7 @@
 #include <wifi_events.h>
 #include <wifi.h>
 
-#define WLAN_DRV_VERSION "v1.3.r51.z_up.p2"
+#define WLAN_DRV_VERSION "v1.3.r51.z_up.p3"
 
 #if CONFIG_WPA2_ENTP
 #include <wm_mbedtls_helper_api.h>
