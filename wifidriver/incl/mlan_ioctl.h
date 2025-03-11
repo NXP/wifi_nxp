a7bce0e26f5d8aec49d35b96dece502e808d9cd2
Manav Chotalia <manavashwinbhai.chotalia@nxp.com>
2025-03-11 13:12:32 +0530

    Fixed STA does not associate to hidden SSID beaconing in DFS channel
    [Description]
    STA does not associate to hidden SSID beaconing in DFS channel
    It associates to hidden SSID beaconing in Non-DFS channel (CH:36)
    
    [Root Cause]
    PASSIVE_TO_ACTIVE scan not enabled for SD8978 and SD8987
    
    [Fix]
    Enabled PASSIVE_TO_ACTIVE support for SD8978 and SD8987
    
    [Testing]
    Validated scan-connect and basic sanity.
    
    [Open Issues/Todo]
    None
    
    [Reviewers]
    Sandip Dalvi, Supriya Inamdar, Abhinav Kulkarni, keval Thakker
    
    Change-Id: ee34e3f8-fe4c-11ef-8238-28d2441bcfa1
    
    Signed-off-by: Manav Chotalia <manavashwinbhai.chotalia@nxp.com>

--- wifidriver/incl/mlan_ioctl.h
+++ wifidriver/incl/mlan_ioctl.h
@@ -276,7 +276,7 @@ typedef enum _mlan_scan_type
     MLAN_SCAN_TYPE_UNCHANGED = 0,
     MLAN_SCAN_TYPE_ACTIVE,
     MLAN_SCAN_TYPE_PASSIVE,
-#if defined(RW610) || defined(SD9177) || defined(IW610)
+#if defined(RW610) || defined(SD9177) || defined(IW610) || defined(SD8978) || defined(SD8987)
     MLAN_SCAN_TYPE_PASSIVE_TO_ACTIVE,
 #endif
 } mlan_scan_type;
