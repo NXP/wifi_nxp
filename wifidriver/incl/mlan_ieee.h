effd7340cbcf7f04252a4c09286a52c1c4be6dea
Fengming Ye <frank.ye@nxp.com>
2025-03-31 12:24:33 +0900

    supplicant case do not need rsno specific buffer
    [Description]
    RSNO driver flow is only needed in embedded supplicant case.
    
    [Root Cause]
    RSNO driver flow is only needed in embedded supplicant case.
    External supplicant will handle RSNO within whole IEs buffer,
    which contains RSNO IEs.
    
    [Fix]
    Wrap driver RSNO buffers and flow with !CONFIG_WPA_SUPP
    
    [Testing]
    Compilation successful and sanity test.
    
    Change-Id: I16933737e3b41c00b4d9db7c1b84b55fddf8f051
    Signed-off-by: Fengming Ye <frank.ye@nxp.com>

--- wifidriver/incl/mlan_ieee.h
+++ wifidriver/incl/mlan_ieee.h
@@ -2310,10 +2310,12 @@ typedef struct _BSSDescriptor_t
     t_u16 wpa_offset;
     /** RSN IE */
     IEEEtypes_Generic_t *prsn_ie;
+#if !CONFIG_WPA_SUPP
     /** RSN Override IE */
     IEEEtypes_Generic_t *prsno_ie;
     /** RSN Override 2 IE */
     IEEEtypes_Generic_t *prsno2_ie;
+#endif
 #ifdef STA_SUPPORT
     /** WAPI IE */
     IEEEtypes_Generic_t *pwapi_ie;
@@ -2377,10 +2379,12 @@ typedef struct _BSSDescriptor_t
     size_t wpa_ie_buff_len;
     unsigned char rsn_ie_buff[MLAN_RSN_MAX_IE_LEN];
     size_t rsn_ie_buff_len;
+#if !CONFIG_WPA_SUPP
     unsigned char rsno_ie_buff[MLAN_WMSDK_MAX_WPA_IE_LEN];
     size_t rsno_ie_buff_len;
     unsigned char rsno2_ie_buff[MLAN_WMSDK_MAX_WPA_IE_LEN];
     size_t rsno2_ie_buff_len;
+#endif
 
     bool wps_IE_exist;
     t_u16 wps_session;
@@ -2389,9 +2393,11 @@ typedef struct _BSSDescriptor_t
     /** RSNX IE */
     IEEEtypes_Rsnx_t *prsnx_ie;
     IEEEtypes_Rsnx_t rsnx_ie_saved;
+#if !CONFIG_WPA_SUPP
     /** RSNX Override IE */
     IEEEtypes_Rsnxo_t *prsnxo_ie;
     IEEEtypes_Rsnxo_t rsnxo_ie_saved;
+#endif
 
     bool brcm_ie_exist;
     bool epigram_ie_exist;
