c6ae885c15108803d8ec44fbfde8a319ae7e6cb5
Fengming Ye <frank.ye@nxp.com>
2025-04-07 18:27:07 +0900

    fix tx power limit to 10 with AP with none country IE
    [Description]
    In embedded supplicant case, when we connect to ext-AP on channel 11
    with none country IE in beacon, we set our tx power limit to 10dB,
    which is 10dB lower than normal cases 20dB.
    
    [Root Cause]
    In wlan_11d_update_chan_pwr_table we first set default tx power limit
    by minmum value of all cfp tables for current channel.
    And then update it due to AP's country IE.
    So if APs' beacon in scan table do not have country IE, we will use
    default value, which is 10dB on channel 11.
    Becase FR cfp table on channel 11 has 10dB power limit.
    But mxdriver does not have such low power limit.
    
    [Fix]
    Update FR cfp table for channel 11.
    Power limit should be 20dB.
    
    [Testing]
    Compilation successful and sanity test.
    
    Change-Id: I69028539366175c3eb02cdf6120be321a553f1bb
    Signed-off-by: Fengming Ye <frank.ye@nxp.com>

--- wifidriver/mlan_cfp.c
+++ wifidriver/mlan_cfp.c
@@ -34,8 +34,6 @@ Change Log:
 #define WLAN_TX_PWR_JP_A_DEFAULT 23
 /** 100mW */
 #define WLAN_TX_PWR_FR_100MW 20
-/** 10mW */
-#define WLAN_TX_PWR_FR_10MW 10
 /** 100mW */
 #define WLAN_TX_PWR_EMEA_DEFAULT 20
 /** 200mW */
@@ -235,9 +233,9 @@ static const chan_freq_power_t channel_freq_power_FR_BG[] = {
     {3, 2422, WLAN_TX_PWR_FR_100MW, (bool)MFALSE}, {4, 2427, WLAN_TX_PWR_FR_100MW, (bool)MFALSE},
     {5, 2432, WLAN_TX_PWR_FR_100MW, (bool)MFALSE}, {6, 2437, WLAN_TX_PWR_FR_100MW, (bool)MFALSE},
     {7, 2442, WLAN_TX_PWR_FR_100MW, (bool)MFALSE}, {8, 2447, WLAN_TX_PWR_FR_100MW, (bool)MFALSE},
-    {9, 2452, WLAN_TX_PWR_FR_100MW, (bool)MFALSE}, {10, 2457, WLAN_TX_PWR_FR_10MW, (bool)MFALSE},
-    {11, 2462, WLAN_TX_PWR_FR_10MW, (bool)MFALSE}, {12, 2467, WLAN_TX_PWR_FR_10MW, (bool)MFALSE},
-    {13, 2472, WLAN_TX_PWR_FR_10MW, (bool)MFALSE}};
+    {9, 2452, WLAN_TX_PWR_FR_100MW, (bool)MFALSE}, {10, 2457, WLAN_TX_PWR_FR_100MW, (bool)MFALSE},
+    {11, 2462, WLAN_TX_PWR_FR_100MW, (bool)MFALSE}, {12, 2467, WLAN_TX_PWR_FR_100MW, (bool)MFALSE},
+    {13, 2472, WLAN_TX_PWR_FR_100MW, (bool)MFALSE}};
 
 
 /** Band: 'B/G', Region: Japan */
