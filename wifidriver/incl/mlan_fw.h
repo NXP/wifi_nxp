ace2f585764de628b2f2af2b6ad54492e24a3f1d
Mayur Mansharamani <mayur.mansharamani@nxp.com>
2025-05-09 12:43:33 +0000

    STAUT-Legacy Roaming | Link lost seen when DUT roaming with UDP traffic
    [Description]
    During legacy roaming in open security link lost is seen when DUT is roaming with UDP traffic.
    
    [Root Cause]
    This issue is seen due to auth frame not being sent by DUT to fw during roaming.
    The reason DUT is not able to send auth frame to fw is becuase UDP traffic is running continuously which holds all the SDIO port to transmit traffic.
    So, when roaming is triggered, wpa_supplicant_main thread is not able to find the chance to inject auth frame to fw via SDIO port as all the SDIO ports are being used to transmit UDP traffic which is executed by tcp_ip thread.
    
    [Fix]
    1) Enable support of 0x0283 cmd in the driver for SD8978 to sent auth frame as cmd based.
    2) Remove 2 bytes during preparation of 0x0283 cmd for SD8978. While preparing 0x0283 cmd for SD8978 extra 2 bytes were present in cmd due to which the tx_pkt_offset info was wrong because of which STA was not able to authenticate to EX-AP.
    
    [Testing]
    1XK as STA is able to establish connection in open, wpa2 and wpa3 security with EX-AP successfully.
    
    [Open Issues/Todo]
    To debug another issue with wpa2 security during roaming where in EAPOL frames are not getting sent.
    
    [Reviewers]
    Gannicus Guo, Sandip Dalvi, Supriya Inamdar, Abhinav Kulkarni, Keval Thakker, Sanjay Yadav, Pankaj Razdan.
    
    Change-Id: 590f127e-2ccf-11f0-9e74-c3c0c2c3686a
    
    Signed-off-by: Mayur Mansharamani <mayur.mansharamani@nxp.com>

--- wifidriver/incl/mlan_fw.h
+++ wifidriver/incl/mlan_fw.h
@@ -727,7 +727,7 @@ typedef enum _WLAN_802_11_WEP_STATUS
 #if defined(SD8987)
 #define FW_CAPINFO_EXT_CMD_TX_DATA          MBIT(29)
 #define IS_FW_SUPPORT_CMD_TX_DATA(_adapter) (_adapter->fw_cap_info & FW_CAPINFO_EXT_CMD_TX_DATA)
-#elif defined(SD9177) || defined(IW610)
+#elif defined(SD9177) || defined(IW610) || defined (SD8978)
 #define FW_CAPINFO_EXT_CMD_TX_DATA          MBIT(16)
 /** Check if transmit mgmt pkt through command supported by firmware */
 #define IS_FW_SUPPORT_CMD_TX_DATA(_adapter) (_adapter->fw_cap_ext & FW_CAPINFO_EXT_CMD_TX_DATA)
