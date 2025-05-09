535264cab8d2497c8576d1bfe46f6f22927252c1
Qiankun Li <qiankun.li@nxp.com>
2025-05-09 16:17:22 +0530

    Add independent reset support for FRDM-MCXN947
    [Description]
    Add independent reset support for FRDM-MCXN947
    
    [Root Cause]
    1. When set independent reset mode as outband mode,
    should pull down and pull up specified GPIO to trigger independent
    reset.
    2. Missing GPIO setting for FRDM-MCXN947.
    
    [Fix]
    1. Specified outband GPIO for FRDM-MCXN947.
    2. Add other enhanced code.
    
    [Port proposal]
    non-general
    
    [Testing]
    1.Independent outband reset works well.
     wlan-set-indrstcfg 1 10
     wlan-independent-reset
    2.Independent inband reset works well.
     wlan-set-indrstcfg 2
     wlan-independent-reset
    
    [Reviewers]
    Gannicus Guo.
    
    Change-Id: I892cbc47e4db01a73330f38424c0ea2231ec1397
    Signed-off-by: Qiankun Li <qiankun.li@nxp.com>

--- wlcmgr/wlan.c
+++ wlcmgr/wlan.c
@@ -131,6 +131,13 @@
 #define IR_OUTBAND_TRIGGER_GPIO_PIN   	(12U)
 #define IR_OUTBAND_TRIGGER_GPIO_NAME  	"GPIO5"
 #endif
+#elif defined(CPU_MCXN947VDF_cm33_core0) || defined(CPU_MCXN947VPB_cm33_core0) // For FRDM-MCXN947
+#if defined(IW610)
+/* IR OUT-BAND TRIGGER GPIO */
+#define IR_OUTBAND_TRIGGER_GPIO          GPIO1
+#define IR_OUTBAND_TRIGGER_GPIO_PIN      (22U)
+#define IR_OUTBAND_TRIGGER_GPIO_NAME     "GPIO1"
+#endif
 #endif /* (defined(CPU_MIMXRT1062DVMAA) || (CPU_MIMXRT1062DVL6A)) */
 #endif
 
@@ -8417,8 +8424,11 @@ int wlan_start(int (*cb)(enum wlan_event_reason reason, void *data))
 
 #if (CONFIG_WIFI_IND_RESET) && (CONFIG_WIFI_IND_DNLD)
 #ifdef IR_OUTBAND_TRIGGER_GPIO
+#if defined(CPU_MCXN947VDF_cm33_core0) || defined(CPU_MCXN947VPB_cm33_core0)
+    gpio_pin_config_t out_config = {kGPIO_DigitalOutput, 1};
+#else
     gpio_pin_config_t out_config = {kGPIO_DigitalOutput, 1, kGPIO_NoIntmode};
-
+#endif
 #if defined(IOMUXC_GPIO_IR_OUTBAND_TRIGGER)
     IOMUXC_SetPinMux(IOMUXC_GPIO_IR_OUTBAND_TRIGGER, /* GPIO_AD_B0_10 is configured as GPIO1_IO10 */
                      0U);
