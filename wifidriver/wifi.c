9b05865f0bf638075b7b9446416905b4275e00eb
Abhinav Kulkarni <abhinav.kulkarni@nxp.com>
2025-05-09 17:32:49 +0530

    Added wpa supplicant feature
    [Description]
    Commented country code updattion temporarily. This is causing hard fault
    and needs to be debugged.
    
    [Root Cause]
    NA
    
    [Fix]
    NA
    
    [Testing]
    Tested supplicant on FC and RB3P
    
    [Open Issues/Todo]
    Need to find root cause and add fix. UAP need to be enabled.
    
    [Reviewers]
    Gannicus Guo, Maochen.
    
    [Port proposal]
    non-general
    
    Change-Id: 4c33262d-821a-4d53-90a6-06c2cbca84e8
    
    Signed-off-by: Abhinav Kulkarni <abhinav.kulkarni@nxp.com>

--- wifidriver/wifi.c
+++ wifidriver/wifi.c
@@ -4750,6 +4750,7 @@ int wifi_set_country_code(const char *alpha2)
     (void)memcpy(pmadapter->country_code, country_code, COUNTRY_CODE_LEN);
 
 #if CONFIG_WPA_SUPP
+#if defined(RW610)
     if (wm_wifi.supp_if_callbk_fns->chan_list_changed_callbk_fn)
     {
 #if CONFIG_WPA_SUPP_AP
@@ -4765,7 +4766,7 @@ int wifi_set_country_code(const char *alpha2)
 #endif
     }
 #endif
-
+#endif
     return WM_SUCCESS;
 }
 
