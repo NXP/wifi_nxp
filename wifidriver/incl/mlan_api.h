5d3bcd893bceaa720270bf4b884a383795332245
Li Long <li.long@nxp.com>
2025-05-20 17:48:18 +0800

    in-UAP+in-STA: UAP Country IE to follow STA connect BSS
    [Description]
    Test step:
    (1) UAP started on country code US.
    (2) STA connect to Ext-AP on country code CN.
    Issue:
    UAP should follow the STA connected bss country code to CN,
    but the UAP beacon IE country code is not changed (still US).
    
    [Root Cause]
    When STA connect to Ext-AP (CN), STA already download
    802_11D_DOMAIN_INFO [0x5b] cmd on both 2G and 5G
    to configure country code and channels.
    But for UAP, beacon still show old country(US), so it need
    fix to download APCMD_SYS_CONFIGURE [0xb0] cmd with
    custom_ie[0x0169] tlv to configure new country related ies.
    
    [Fix]
    When STA assoc, get COUNTRY_INFO[0x07] IE from scan bss.
    For each used index for custom ie:
    Get from UAP with APCMD_SYS_CONFIGURE [0xb0] cmd with
    mgmt_ie[0x0169] tlv with this index.
    Get custom_ie payload and parse the IE:
    if find COUNTRY_INFO[0x07] IE then replace to new
    COUNTRY_INFO[0x07] IE, keep other IEs unchanged.
    Set to UAP with APCMD_SYS_CONFIGURE [0xb0] cmd with
    mgmt_ie[0x0169] tlv with this index and the new custom_ie
    payload.
    If not found from all used index:
    Get an available index to configure the custom IE with the
    new country IE.
    
    [Testing]
    ok
    
    [Reviewers]
    Gannicus Guo
    
    [Port proposal]
    general
    
    Change-Id: I3929391ce8c9afb4d125160aab0d232518f6243c
    Signed-off-by: Li Long <li.long@nxp.com>

--- wifidriver/incl/mlan_api.h
+++ wifidriver/incl/mlan_api.h
@@ -503,6 +503,8 @@ int wifi_set_custom_ie(custom_ie *beacon_ies_data,
                        custom_ie *assocresp_ies_data);
 #endif
 
+unsigned int get_ie_index();
+
 #if CONFIG_11K
 /**
  * rrm scan callback function to process scan results
@@ -580,6 +582,8 @@ int wrapper_bssdesc_second_set(int bss_index,
 int wifi_get_mgmt_ie2(mlan_bss_type bss_type, void *buf, unsigned int *buf_len);
 int wifi_set_mgmt_ie2(mlan_bss_type bss_type, unsigned short mask, void *buf, unsigned int buf_len);
 int wifi_clear_mgmt_ie2(mlan_bss_type bss_type, int mgmt_bitmap_index);
+
+int wifi_get_mgmt_ie_by_index(mlan_bss_type bss_type, void *buffer, unsigned int *ie_len, int index);
 
 #if CONFIG_BG_SCAN
 int wifi_request_bgscan(mlan_private *pmpriv);
