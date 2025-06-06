e1127c5bda5d6cef3ea89f2cb27bdc2ee759a288
Keval Thakker <keval.thakker@nxp.com>
2025-06-06 14:51:57 +0000

    [Firecrest RTOS][P2P][Stress] Ex-P2P-Client fails to get IP address on 6th iterations.
    [Description]
    Ex-P2P-Client fails to get IP address on 6th iterations.
    
    [Root Cause]
    wlan.cur_network_idx was getting incremented and thus wlan.networks array was getting filled up on every connection.
    
    [Fix]
    Added logic to reset the network name, when p2p-group-remove event is received.
    
    [Testing]
    Verified the below sequence 10 times:
    - wlan-p2p-find
    - wlan-p2p-connect <AP's mac> pbc join
    - wlan-p2p-group-remove wf3
    - wlan-disconnect
    
    [Open Issues/Todo]
    None.
    
    [Reviewers]
    Gannicus Guo, Supriya Inamdar, Sanjay Yadav, Pankaj Razdan.
    
    Change-Id: bac44404-42e5-11f0-8c3c-00155d9de4dd
    
    Signed-off-by: Keval Thakker <keval.thakker@nxp.com>

--- wlcmgr/wlan.c
+++ wlcmgr/wlan.c
@@ -5665,6 +5665,12 @@ static void wpa_supplicant_msg_cb(const char *buf, size_t len)
         else if (strstr(buf, " client "))
         {
             priv_wfd->p2p_gc_network = false;
+            if(wlan.networks[wlan.cur_network_idx].name[0] != '\0')
+            {
+                memset(wlan.networks[wlan.cur_network_idx].name, 0 , WLAN_NETWORK_NAME_MAX_LENGTH + 1);
+                memset(wlan.networks[wlan.cur_network_idx].ssid, 0 , IEEEtypes_SSID_SIZE + 1);
+                memset(wlan.networks[wlan.cur_network_idx].bssid, 0 , IEEEtypes_ADDRESS_SIZE);
+            }
         }
     }
     else
