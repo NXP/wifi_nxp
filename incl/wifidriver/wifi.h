329971e97a85206deb4d9103c7b2793bbe596828
Abhinav Kulkarni <abhinav.kulkarni@nxp.com>
2025-07-02 09:57:17 +0000

    Fix command timeout in RFTM for IW416
        [Description]
        Command response to seen for IW416 while enabling RFTM
    
        [Root Cause]
        VDLL support is needed in latest fw
    
        [Fix]
        Enabled VDLL for IW416
    
        [Testing]
        Build success and checked for no command response to
    
        [Open Issues/Todo]
        None
    
        [Reviewers]
        Gannicus Guo, Maochen W
    
        [Port proposal]
        non-general
    
        Change-Id: 02b6fc36-7d5b-45e8-af99-db715a404ce3
    
    Signed-off-by: Abhinav Kulkarni <abhinav.kulkarni@nxp.com>

--- incl/wifidriver/wifi.h
+++ incl/wifidriver/wifi.h
@@ -24,7 +24,7 @@
 #if defined(SD9177)
 #define CONFIG_TCP_ACK_ENH 1
 #endif
-#if defined(SD9177) || defined(IW610)
+#if defined(SD9177) || defined(IW610) || defined(SD8978)
 #define CONFIG_FW_VDLL     1
 #if !CONFIG_WIFI_CAPA
 #undef CONFIG_WIFI_CAPA
