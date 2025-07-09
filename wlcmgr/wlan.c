9d6598f87de8213755a164ba24568727c633defb
Maochen Wang <maochen.wang@nxp.com>
2025-07-09 17:38:41 +0800

    IW610 config aligns with RW612 for Zephyr.
    [Description]
    Fix some issues when IW610 config aligns with RW612 for Zephyr.
    When CONFIG_WIFI_RF_TEST_MODE is enabled, basic nxp_wifi cmd is not supported.
    
    [Root Cause]
    Fix some issues when IW610 config aligns with RW612 for Zephyr.
    When CONFIG_WIFI_RF_TEST_MODE is enabled, basic nxp_wifi cmd is not supported.
    
    [Fix]
    wifi_cal_data_rw61x_override.h is not used anymore, and it is replaced by wifi_cal_data_override.h.
    When CONFIG_WIFI_RF_TEST_MODE is enabled, should also register basic cli cmd for IW610.
    
    [Testing]
    Done.
    
    [Open Issues/Todo]
    build pass
    
    [Reviewers]
    Gannicus Guo
    
    [Port proposal]
    non-general
    
    Change-Id: 77ce5c48-5ca8-11f0-92c8-bfceb7e55f53
    Signed-off-by: Maochen Wang <maochen.wang@nxp.com>

--- wlcmgr/wlan.c
+++ wlcmgr/wlan.c
@@ -8295,7 +8295,7 @@ int wlan_start(int (*cb)(enum wlan_event_reason reason, void *data))
         return 0;
     }
 #endif
-#if !(CONFIG_WIFI_RF_TEST_MODE) || (CONFIG_NXP_RW610)
+#if !(CONFIG_WIFI_RF_TEST_MODE) || (CONFIG_NXP_RW610) || (CONFIG_NXP_IW610)
     ret = wlan_basic_cli_init();
     if (ret != WM_SUCCESS)
     {
