a0d1fa6caeccdb34a45a4cc90d14b8920310f50c
Keval Thakker <keval.thakker@nxp.com>
2025-08-06 10:59:42 +0000

    Compilation error with Wi-Fi deug macros
    [Description]
    Compilation error with Wi-Fi debug macros
    
    [Root Cause]
    key_mgmt_ie_rsno2 variable not gaurded by CONFIG_WPA_SUPP macro.
    
    [Fix]
    Added guarding macro CONFIG_WPA_SUPP around key_mgmt_ie_rsno and key_mgmt_ie_rsno2 variables.
    
    [Testing]
    Build successful for wpa_supplicant app using armgcc and mcuX compilers with CONFIG_WIFI_EXTRA_DEBUG enabled.
    
    [Open Issues/Todo]
    NA.
    
    Change-Id: 499a035a-72b4-11f0-8ad7-00155d113708
    
    [Reviewers]
    Gannicus Guo, Sandip Dalvi, Supriya Inamdar, Pankaj Razdan.
    
    Signed-off-by: Keval Thakker <keval.thakker@nxp.com>

--- wifidriver/mlan_glue.c
+++ wifidriver/mlan_glue.c
@@ -2123,9 +2123,11 @@ static int wifi_assoc_pick_security_ie(mlan_private *priv, BSSDescriptor_t *d,
         goto wpa_ie_picked;
     }
 
+#if !CONFIG_WPA_SUPP
     wifi_d("wifi assoc select none security profile, "
            "key mgmt network 0x%x rsno2 0x%x rsno 0x%x rsn 0x%x",
            key_mgmt_network, key_mgmt_ie_rsno2, key_mgmt_ie_rsno, key_mgmt_ie_rsn);
+#endif
     priv->sec_info.rsn_selector = MLAN_RSN_SELECTOR_INVALID;
     return WM_SUCCESS;
 
@@ -2150,10 +2152,11 @@ rsn_ie_picked:
     priv->sec_info.is_wpa_tkip  = is_wpa_tkip;
     priv->sec_info.wpa2_enabled = true;
 
+#if !CONFIG_WPA_SUPP
     wifi_d("assoc_pick_rsn: "
            "key mgmt network 0x%x rsno2 0x%x rsno 0x%x rsn 0x%x",
            key_mgmt_network, key_mgmt_ie_rsno2, key_mgmt_ie_rsno, key_mgmt_ie_rsn);
-
+#endif
 #if CONFIG_11R
     if (wlan_security == WLAN_SECURITY_WPA2 || wlan_security == WLAN_SECURITY_WPA3_SAE ||
         wlan_security == WLAN_SECURITY_WPA2_WPA3_SAE_MIXED || wlan_security == WLAN_SECURITY_WPA2_FT)
