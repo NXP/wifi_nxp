9d122f6ac74329623fb9b9386db44f730930261c
Keval Thakker <keval.thakker@nxp.com>
2025-12-02 18:08:17 +0000

    STAUT is not sending SA Query Response to AP's correctly protected SA Query Request
    [Description]
    STAUT is not sending SA Query Response to AP's correctly protected SA Query Request
    
    [Root Cause]
    Action frame sub-type was removed while sending management(assoc) frame.
    
    [Fix]
    Register ACTION frames for rx management indications for sta interface at wpa_supplicant dev init.
    
    [Testing]
    Executed connection with wpa2 security and verified SA query response going successfully.
    
    [Open Issues/Todo]
    None
    
    [Port proposal]
    General
    
    [Reviewers]
    Abhinav K, Gannicus G, Supriya I, Qingling W.
    
    Change-Id: d51be342-cfa9-11f0-a606-00155dafb08d
    
    Signed-off-by: Keval Thakker <keval.thakker@nxp.com>

--- wifidriver/wpa_supp_if/rtos_wpa_supp_if.c
+++ wifidriver/wpa_supp_if/rtos_wpa_supp_if.c
@@ -647,12 +647,15 @@ void *wifi_nxp_wpa_supp_dev_init(void *supp_drv_if_ctx,
     }
 
 #if CONFIG_WPA_SUPP_P2P
-    if (wifi_if_ctx_rtos->bss_type == BSS_TYPE_WFD)
+    if ((wifi_if_ctx_rtos->bss_type == BSS_TYPE_STA)
+       || (wifi_if_ctx_rtos->bss_type == BSS_TYPE_WFD))
+#else
+    if (wifi_if_ctx_rtos->bss_type == BSS_TYPE_STA)
+#endif
     {
         t_u32 mgmt_subtype_mask = WLAN_MGMT_ACTION;
         (void)wifi_set_rx_mgmt_indication(wifi_if_ctx_rtos->bss_type, mgmt_subtype_mask);
     }
-#endif
 
     memcpy(&wifi_if_ctx_rtos->supp_callbk_fns, supp_callbk_fns, sizeof(wifi_if_ctx_rtos->supp_callbk_fns));
     return wifi_if_ctx_rtos;
