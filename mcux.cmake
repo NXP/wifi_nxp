e8597e99f5ac3949c41d474ae7e5e4cc56cf4f00
Qingling Wu <qingling.wu@nxp.com>
2025-12-22 16:11:41 +0800

    Sync position of firmware files
    [Description]
    Sync position of firmware for rw61x and iw610x
    
    [Root Cause]
    Sync position of firmware for rw61x and iw610x
    
    [Fix]
    Sync position of firmware for rw61x and iw610x
    
    [Testing]
    Local compile ok
    
    [Reviewers]
    Gannicus Guo, Supriya Sumeet Inamdar, Mayur Mansharamani
    
    [Port proposal]
    non-general
    
    Change-Id: 7db7dcbadf0711f0ae2fab720ebae8b2
    
    Signed-off-by: Qingling Wu <qingling.wu@nxp.com>

--- mcux.cmake
+++ mcux.cmake
@@ -184,32 +184,6 @@ if (CONFIG_MCUX_COMPONENT_middleware.wifi.fwdnld)
                 # TODO please change to relative dir
                 wifi_bt_firmware/nw61x/nw61x_cpu2_se.c
                 # TODO please change to relative dir
-                wifi_bt_firmware/iw610/sduartspi_iw610.bin.se.inc
-                # TODO please change to relative dir
-                wifi_bt_firmware/iw610/sd_iw610.bin.se.inc
-                # TODO please change to relative dir
-                wifi_bt_firmware/iw610/uartspi_iw610.bin.se.inc
-                wifi_bt_firmware/iw610/sduart_iw610.bin.se.inc
-                wifi_bt_firmware/iw610/uart_iw610_bt.bin.se.inc
-                # TODO please change to relative dir
-                wifi_bt_firmware/iw610/sduart_iw610_se.h
-                # TODO please change to relative dir
-                wifi_bt_firmware/iw610/sd_iw610_se.h
-                # TODO please change to relative dir
-                wifi_bt_firmware/iw610/uart_iw610_se.h
-                # TODO please change to relative dir
-                wifi_bt_firmware/iw610/iw610_cpu12_se.c
-                # TODO please change to relative dir
-                wifi_bt_firmware/iw610/iw610_cpu2_se.c
-                # TODO please change to relative dir
-                wifi_bt_firmware/iw610/iw610_cpu1_se.c
-                # TODO please change to relative dir
-                wifi_bt_firmware/iw610/sd_iw610_compressed.bin.se.inc
-                wifi_bt_firmware/iw610/sduart_iw610_compressed.bin.se.inc
-                wifi_bt_firmware/iw610/sduartspi_iw610_compressed.bin.se.inc
-                wifi_bt_firmware/iw610/uart_iw610_bt_compressed.bin.se.inc
-                wifi_bt_firmware/iw610/uartspi_iw610_compressed.bin.se.inc
-                # TODO please change to relative dir
                 wifidriver/sdio.c
                 # TODO please change to relative dir
                 wifidriver/sdio.h
