be3c38d0546c4c717094713edaaa4fe3007168f2
Fengming Ye <frank.ye@nxp.com>
2025-12-30 14:41:53 +0900

    fix txpd size 28 tcp tx fail in SD zero copy case
    [Description]
    In SD zero copy case, TCP TX fail when change txpd size to 28.
    
    [Root Cause]
    When txpd size is 28, TCP TX has some packet with size 0x500.
    Then sg_data_tx_prepare meant to add padding round up 0x100 multiples.
    For 0x500 it should not but add padding to 0x600.
    So the interface header size field is 0x500, but SDHC transfer size
    is 0x600. This will cause SD card receive error
    and then driver mp_wr_bitmap error.
    
    [Fix]
    Only add padding when packet size is not 0x100 multiples.
    
    [Testing]
    Build and sanity test pass.
    
    [Reviewers]
    Gannicus Guo
    
    [Port proposal]
    Y
    
    Change-Id: I60cbf2a41c6bf6e750cd95f279df5f352e256829
    Signed-off-by: Fengming Ye <frank.ye@nxp.com>

--- wifidriver/wifi-sdio.c
+++ wifidriver/wifi-sdio.c
@@ -623,7 +623,10 @@ static sg_data_list_t *sg_data_tx_prepare(t_u8 *out_buf)
     }
 
     /* 12. add sdio block size padding */
-    SG_DATA_SIZE(tail) += MLAN_SDIO_BLOCK_SIZE - (pkt_len & (MLAN_SDIO_BLOCK_SIZE - 1));
+    if ((pkt_len & (MLAN_SDIO_BLOCK_SIZE - 1)) != 0)
+    {
+        SG_DATA_SIZE(tail) += MLAN_SDIO_BLOCK_SIZE - (pkt_len & (MLAN_SDIO_BLOCK_SIZE - 1));
+    }
     return head;
 
 clone:
@@ -698,7 +701,10 @@ skip_alloc:
     }
 
     /* 17. add sdio block size padding */
-    SG_DATA_SIZE(tail) += MLAN_SDIO_BLOCK_SIZE - (pkt_len & (MLAN_SDIO_BLOCK_SIZE - 1));
+    if ((pkt_len & (MLAN_SDIO_BLOCK_SIZE - 1)) != 0)
+    {
+        SG_DATA_SIZE(tail) += MLAN_SDIO_BLOCK_SIZE - (pkt_len & (MLAN_SDIO_BLOCK_SIZE - 1));
+    }
     return head;
 
 fail:
