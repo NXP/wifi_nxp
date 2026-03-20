6bd4d49a2112c41f15fc54d27671f51d7d0834be
Li Long <li.long@nxp.com>
2026-03-20 15:38:51 +0800

    Fix SDIO cmd copy to outbuf may lost1-4 bytes
    [Description]
    DUT with 5G AP WPS connect and disconnect loop test on Zephyr:
    after 3-5 times loop, supplicant layer scan timeout (Not 0x107 tmo).
    
    [Root Cause]
    The reason is driver download channel  scan tlv with min/max scan
    time sometimes is large: eg, 28s 48s.
    This is easily seen on ch1/ch6 0x107 scan cmd (cmd total 256 bytes).
    When time is large, FW keeps report 0x58 event and 0x8107 cmdrsp
    rx on host very late after 18s.
    From host driver side:
    The scan cmd in global cmd buffer ctrl_cmd_buf content is right and
    is placed after reserved INTF_HDR(4) bytes in cmd buf.
    On SDIO layer will copy cmd buffer to outbuf by calculated multiple
    blocks count copy.
    The copy incude SDIO hdr size but block count calulation not include
    SDIO hdr size, then the later memcpy will list 1-4 bytes when cmd
    size happen to be multiple of block size.
    This is only seen on Zephyr, FreeRTOS driver already include this
    fix.
    
    [Fix]
    Incude SDIO hdr size when do block count calulation.
    
    [Testing]
    ok
    
    [Reviewers]
    Gannicus Guo
    
    [Port proposal]
    non-general
    
    Change-Id: I8afdcf9bace96b2c6b5eae203c60abb2505edb2b
    Signed-off-by: Li Long <li.long@nxp.com>

--- wifidriver/wifi.c
+++ wifidriver/wifi.c
@@ -594,7 +594,8 @@ resend:
     }
 
 #ifndef RW610
-    tx_blocks = ((t_u32)cmd->size + MLAN_SDIO_BLOCK_SIZE - 1U) / MLAN_SDIO_BLOCK_SIZE;
+    /* First 4 bytes reserved for SDIO pkt header */
+    tx_blocks = ((t_u32)cmd->size + INTF_HEADER_LEN + MLAN_SDIO_BLOCK_SIZE - 1U) / MLAN_SDIO_BLOCK_SIZE;
 #endif
 
 #if !CONFIG_UART_WIFI_BRIDGE
