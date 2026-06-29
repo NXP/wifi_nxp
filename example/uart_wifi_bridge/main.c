3001a9f0ff611f1da81fde1882c6566aa42bdf61
Sanjay Yadav <sanjay.yadav@nxp.com>
2026-06-29 18:41:04 +0530

    Add mimxrt2660evk support for uart_wifi_bridge
    [Description]
    Added mimxrt2660evk board support for uart_wifi_bridge
    
    [Root Cause]
    NA
    
    [Fix]
    Added mimxrt2660evk specific macros
    
    [Port Proposal]
    Non-general.
    
    [Testing]
    Compilation Success.
    
    Change-Id: e42f50ba-73bb-11f1-835b-00155dea7b68
    
    Signed-off-by: Sanjay Yadav <sanjay.yadav@nxp.com>

--- example/uart_wifi_bridge/main.c
+++ example/uart_wifi_bridge/main.c
@@ -66,6 +66,10 @@
 #define DEMO_LPUART          LPUART4
 #define DEMO_LPUART_CLK_FREQ BOARD_DEBUG_UART_CLK_FREQ
 #define DEMO_LPUART_IRQn     LP_FLEXCOMM4_IRQn
+#elif defined(MIMXRT2663_SERIES)
+#define DEMO_LPUART          HSP__LPUART_1
+#define DEMO_LPUART_CLK_FREQ BOARD_DebugConsoleSrcFreq()
+#define DEMO_LPUART_IRQn     HSP_LPUART1_IRQn
 #elif !defined(RW610_SERIES) && !defined(RW612_SERIES)
 #define DEMO_LPUART          LPUART1
 #define DEMO_LPUART_CLK_FREQ BOARD_DebugConsoleSrcFreq()
@@ -1474,6 +1478,8 @@ static void main_task(osa_task_param_t arg)
 #else
 #if defined(MCXN947_cm33_core0_SERIES)
     (void)NVIC_SetPriority(LP_FLEXCOMM4_IRQn, 5);
+#elif defined(MIMXRT2663_SERIES)
+    (void)NVIC_SetPriority(HSP_LPUART1_IRQn, 5);
 #else
     (void)NVIC_SetPriority(LPUART1_IRQn, 5);
 #endif
@@ -1481,6 +1487,8 @@ static void main_task(osa_task_param_t arg)
     (void)NVIC_SetPriority(LPUART2_IRQn, HAL_UART_ISR_PRIORITY);
 #elif defined(MCXN947_cm33_core0_SERIES)
     (void)NVIC_SetPriority(LP_FLEXCOMM2_IRQn, HAL_UART_ISR_PRIORITY);
+#elif defined(MIMXRT2663_SERIES)
+    (void)NVIC_SetPriority(HSP_LPUART1_IRQn, HAL_UART_ISR_PRIORITY);
 #else
     (void)NVIC_SetPriority(LPUART3_IRQn, HAL_UART_ISR_PRIORITY);
 
@@ -1498,6 +1506,8 @@ static void main_task(osa_task_param_t arg)
     lpuart_config_bt.srcclk = BOARD_BT_UART_CLK_FREQ;
 #if defined(MIMXRT1176_cm7_SERIES) || defined(MCXN947_cm33_core0_SERIES)
     lpuart_config_bt.base   = LPUART2;
+#elif defined(MIMXRT2663_SERIES)
+    lpuart_config_bt.base   = HSP__LPUART_3;
 #else
     lpuart_config_bt.base = LPUART3;
 #endif
